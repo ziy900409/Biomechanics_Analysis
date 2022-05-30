c82347505bcf92ccb1f6e46dd3d9f5cf843fe351
Yang Hsin <45969661+ziy900409@users.noreply.github.com>
2022-05-30 12:03:22 +0800

    Update and rename Read_CSV_file.py to Read_file_path.py

--- OtherFunction/Read_file_path.py
+++ OtherFunction/Read_file_path.py
@@ -7,28 +7,47 @@ It can be used to different type of file, please change line 23, 30 as needed
 
 # using a recursive loop to traverse each folder
 # and find the file extension has .csv
-def Read_File(x, subfolder='None'):
+def Read_File(x, y, subfolder='None'):
+    '''
+    This function will return file path
+    
+
+    Parameters
+    ----------
+    x : str
+        file path
+    y : str
+        file type
+        example: ".csv"
+    subfolder : boolean
+    
+    Returns
+    -------
+    csv_file_list : list
+    '''
+    
     # if subfolder = True, the function will run with subfolder
     folder_path = x
+    data_type = y
     csv_file_list = []
     
     if subfolder:
         file_list_1 = []
         for dirPath, dirNames, fileNames in os.walk(x):
+            # file_list = os.walk(folder_name)
             file_list_1.append(dirPath)
+        # need to change here [1:]
         for ii in file_list_1[1:]:
             file_list = os.listdir(ii)
             for iii in file_list:
-              # change .CSV as needed
-                if os.path.splitext(iii)[1] == ".csv":
+                if os.path.splitext(iii)[1] == data_type:
                     file_list_name = ii + '\\' + iii
                     csv_file_list.append(file_list_name)
     else:
         folder_list = os.listdir(x)                
         for i in folder_list:
-          # change .CSV as needed
-            if os.path.splitext(i)[1] == ".csv":
+            if os.path.splitext(i)[1] == data_type:
                 file_list_name = folder_path + "\\" + i
-                csv_file_list.append(i)                
+                csv_file_list.append(file_list_name)                
         
     return csv_file_list
